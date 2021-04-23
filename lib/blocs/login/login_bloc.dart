a160c0efd3cebbe6e0631c900a335c99074fdb47
kauli-sabino <kaulisabino27@gmail.com>
2021-04-23 00:08:13 -0300

    fix login bloc

--- lib/blocs/login/login_bloc.dart
+++ lib/blocs/login/login_bloc.dart
@@ -1,15 +1,19 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:pi_mobile/model/user_model.dart';
 import 'package:pi_mobile/resources/login_connect.dart';
-import 'package:pi_mobile/utils.dart';
 import 'package:rxdart/subjects.dart';
 
+enum LoginEvent { loading, done, error, waiting }
+
 class LoginBloc extends BlocBase {
+  LoginEvent loginState = LoginEvent.waiting;
+  GlobalKey<NavigatorState> navigatorKey;
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   TextEditingController email = TextEditingController();
+  BuildContext loginContext;
+  BuildContext modalCtxt;
   TextEditingController password = TextEditingController();
   BehaviorSubject _controller = BehaviorSubject();
   bool validate_email = false;
@@ -19,35 +23,82 @@ class LoginBloc extends BlocBase {
 
   Sink get input => _controller.sink;
   Stream get output => _controller.stream;
+
   //we have fix this scrap
-  void login(BuildContext loginContext, GlobalKey<NavigatorState> navigatorKey) async {
-    // if (formKey.currentState.validate()) {
-    //   String route = '/login';
-    //   userModel.email = email.text;
-    //   userModel.password = password.text;
-    //   try {
-    //     var loginConnect = await LoginConnect.connect(userModel);
-    //     if (loginConnect is DioError) {
-    //       throw 'error';
-    //     }
-    //     navigatorKey.currentState.pushNamed('/');
+  void login(BuildContext newLoginContext, GlobalKey<NavigatorState> newNavigatorKey) async {
+    navigatorKey = newNavigatorKey;
 
-    //     // Utils.navigatorKey.currentState.pushNamed('/');
-    //   } catch (e) {
-    //     route = '/login';
-    //     validate_email = true;
-    //     validate_password = true;
-    //     formKey.currentState.validate();
+    loginContext = newLoginContext;
+    if (formKey.currentState.validate()) {
+      userModel.email = email.text;
+      userModel.password = password.text;
+      loginState = LoginEvent.loading;
+      input.add(userModel);
+      try {
+        var loginConnect = await LoginConnect.connect(userModel);
+        if (loginConnect is DioError) {
+          throw 'error';
+        }
+        loginState = LoginEvent.done;
+      } catch (e) {
+        validate_email = true;
+        validate_password = true;
+        formKey.currentState.validate();
+        loginState = LoginEvent.error;
 
-    //     print('wrong credentials');
-    //   }
-    input.add(userModel);
-    // }
+        print('wrong credentials');
+      }
+      input.add(userModel);
+    }
   }
 
   @override
   void dispose() {
     _controller.close();
     super.dispose();
+  }
+
+  LoginBloc() {
+    output.listen((event) {
+      mapEventToState(loginState);
+    });
+  }
+
+  void mapEventToState(LoginEvent evt) {
+    if (evt == LoginEvent.done) {
+      Navigator.pop(modalCtxt);
+
+      print('pronto pra redirecionar');
+
+      navigatorKey.currentState.pushNamed('/');
+    }
+    if (evt == LoginEvent.waiting) {
+      print('esperando um event ...');
+    }
+    if (evt == LoginEvent.error) {
+      print('credenciais erradas');
+      Navigator.pop(modalCtxt);
+    }
+    if (evt == LoginEvent.loading) {
+      print('carregando...');
+      showGeneralDialog(
+          context: loginContext,
+          pageBuilder: (ctxt, _, __) {
+            modalCtxt = ctxt;
+            print('largura da parada ${MediaQuery.of(loginContext).size.width * 0.5}');
+            print('altura da parada ${MediaQuery.of(loginContext).size.width * 0.5}');
+            return Scaffold(
+              backgroundColor: Colors.transparent,
+              body: Center(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(maxWidth: MediaQuery.of(loginContext).size.width * 0.1, minHeight: MediaQuery.of(loginContext).size.width * 0.1),
+                  // width: MediaQuery.of(loginContext).size.width * 0.1,
+                  // height: MediaQuery.of(loginContext).size.width * 0.1,
+                  child: Center(child: CircularProgressIndicator()),
+                ),
+              ),
+            );
+          });
+    }
   }
 }
